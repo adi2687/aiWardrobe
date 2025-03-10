8d3fc51989af7db3a25e230ca4cef7887ed70e5b
Aditya <adityakurani26@gmail.com>
2025-03-10 20:48:55 +0530

    Added profile option , login ,resgter but it is on json format only , can be seen in the html pages as of now

---
+++ backend/model/user.js
@@ -0,0 +1,14 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema=new mongoose.Schema({
+    username:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    profileImageURL:{type:String , default:'./profileImage/default.png'}
+},{timestamps:true})
+
+const User=mongoose.model('User',userSchema)
+
+export default User
