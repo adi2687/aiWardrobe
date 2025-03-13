b5c4c261c13b0c0dbeccf01445bbc22b2bd88f61
Aditya <adityakurani26@gmail.com>
2025-03-13 12:16:14 +0530

    not much chngae

--- frontend/src/components/apifetch.jsx
+++ frontend/src/components/apifetch.jsx
@@ -9,11 +9,9 @@ function App() {
       credentials: "include", 
     })
       .then((response) => {
-        console.log("Raw Response:", response);
         return response.json();
       })
       .then((data) => {
-        console.log("Received Data:", data);
         setData(data);
       })
       .catch((error) => console.error("Error fetching data:", error));
@@ -23,26 +21,7 @@ function App() {
 
   return (
     <div>
-      {/* <h1>React + Node.js Connection</h1> */}
-      {/* {data ? (
-        <div>
-          <p>
-            <strong>Message:</strong> {data.message}
-          </p>
-          {data.user && (
-            <div>
-              <p>
-                <strong>Username:</strong> {data.user.username}
-              </p>
-              <p>
-                <strong>Email:</strong> {data.user.email}
-              </p>
-            </div>
-          )}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )} */}
+      
 
 
       {data ? (
