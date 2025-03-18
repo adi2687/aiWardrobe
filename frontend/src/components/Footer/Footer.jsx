e6f75cf5f144bbb9fe2f966a805da49830cd2f8d
Aditya <adityakurani26@gmail.com>
2025-03-18 18:14:51 +0530

    added footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Footer.css";
+import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-logo">
+          <h2>Outfit AI</h2>
+        </div>
+        <div className="footer-links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/features">Features</a>
+          <a href="/contact">Contact</a>
+        </div>
+        <div className="footer-social">
+          <a href="https://www.facebook.com/aditya.kurani.1"><FaFacebookF /></a>
+          <a href="https://www.instagram.com/aditya_kurani_26/"><FaInstagram /></a>
+          <a href="https://x.com/AdityaKurani"><FaTwitter /></a>
+          <a href="https://www.linkedin.com/in/aditya-kurani-818668176/"><FaLinkedin /></a>
+        </div>
+      </div>
+      
+      <h3>Made by <a href="https://www.linkedin.com/in/aditya-kurani-818668176/">Aditya Kurani</a> and <a href="https://linkedin.com/in/paras-rana-696b7731b/">Paras Rana</a></h3>
+      <div className="footer-bottom">
+         
+        © {new Date().getFullYear()} Outfit AI. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
