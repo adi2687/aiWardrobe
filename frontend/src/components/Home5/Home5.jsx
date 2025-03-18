a5a35106bf81f858abf08af10a6ecc2bda612abe
Aditya <adityakurani26@gmail.com>
2025-03-18 17:55:18 +0530

    added 5th page

--- frontend/src/components/Home5/Home5.jsx
+++ frontend/src/components/Home5/Home5.jsx
@@ -2,34 +2,24 @@ import React, { useEffect, useRef } from "react";
 import "./Home5.css";
 
 const Home5 = () => {
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-        }
-      },
-      { threshold: 0.3 }
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-    };
-  }, []);
 
   return (
-    <div className="home5-container" ref={sectionRef}>
-      <div className="quote">Ditch the Guesswork—Let AI Define Your Look.</div>
+    <div className="home5-container" >
+      <div className="quotehome5">Ditch the Guesswork—Let AI Define Your Look.</div>
       <div className="cta-button">
         <button>Try Now</button>
+      </div>
+      <div className="features_page5">
+        <div>Weather based recommendations</div>
+        <div>Daily outfit planner</div>
+        <div>Sell your old clothes </div>
+        <div>Accessories guider</div>
+        {/* <br /> */}
+        <div>Augmented Reality (AR)</div>
+        <div>AI-Generated Fashion Reels</div>
+        {/* <div> Fashion Community & Social Feed</div> */}
+        <div>Location-Based Outfit Suggestions</div>
+        {/* <div>AR Prev</div> */}
       </div>
     </div>
   );
