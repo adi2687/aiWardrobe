9a4d8e895f29f1ee7057b64654b2db671c7af17f
Aditya <adityakurani26@gmail.com>
2025-04-04 01:50:25 +0530

    added the axios ig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  assetsInclude: ['**/*.glb']
-})
+  plugins: [react(), tailwindcss()],
+  assetsInclude: ['**/*.glb'],
+  optimizeDeps: {
+    include: ['axios']
+  },
+  build: {
+    rollupOptions: {
+      external: ['axios']
+    }
+  }
+});
